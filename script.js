4563c73e8bafb6ff791218b428a3a220edb8ec0c
Gaurav Pande <141733951+AgentRoss@users.noreply.github.com>
2024-04-13 15:56:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,71 @@
+const colorPicker = document.getElementById("colorPicker");
+const canvasColor = document.getElementById("canvasColor");
+const canvas = document.getElementById("myCanvas");
+const clearButton = document.getElementById("clearButton");
+const saveButton = document.getElementById("saveButton");
+const reterievebtn = document.getElementById("retrieveButton");
+const fontPicker = document.getElementById("fontSize");
+
+const ctx = canvas.getContext("2d");
+
+colorPicker.addEventListener("change", (e) => {
+  ctx.strokestyle = e.target.value;
+  ctx.fillStyle = e.target.value;
+});
+
+canvas.addEventListener("mousedown", (e) => {
+  isDrawing = true;
+  lastX = event.offsetX;
+  lastY = event.offsetY;
+});
+
+canvas.addEventListener("mousemove", (e) => {
+  if (isDrawing) {
+    ctx.beginPath();
+    ctx.moveTo(lastX, lastY);
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+
+    lastX = event.offsetX;
+    lastY = event.offsetY;
+  }
+});
+
+canvas.addEventListener("mouseup", () => {
+  isDrawing = false;
+});
+
+canvasColor.addEventListener("change", (e) => {
+  ctx.fillStyle = e.target.value;
+  ctx.fillRect(0, 0, 800, 500);
+});
+
+fontPicker.addEventListener("change", (e) => {
+  ctx.lineWidth = e.target.value;
+});
+
+clearButton.addEventListener("click", () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
+
+saveButton.addEventListener("click", () => {
+  localStorage.setItem("canvasContents", canvas.toDataURL());
+
+  let link = document.createElement("a");
+
+  link.download = "my-canvas.png";
+
+  link.href = canvas.toDataURL();
+
+  link.click();
+});
+
+reterievebtn.addEventListener("click", () => {
+  let savedCanvas = localStorage.getItem("canvasContents");
+
+  if (savedCanvas) {
+    let img = new Image();
+    img.src = savedCanvas;
+    ctx.drawImage(img, 0, 0);
+  }
+});
